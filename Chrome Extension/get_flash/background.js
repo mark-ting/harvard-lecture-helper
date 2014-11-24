76a1f76c397193c9e2884e9ac929c6ffa9f5ab86
goldenratio1618 <aaatanas@yahoo.com>
2014-11-24 02:49:34 -0500

    Revert "Milestone 1"
    This reverts commit ff2d5c0edecf3088090186a3b2404237e2ecadef.

--- Chrome Extension/get_flash/background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log("Injecting code...");
-    chrome.tabs.executeScript(null, {file: "scripts.js"});
-    console.log("Injected code!");
-});
