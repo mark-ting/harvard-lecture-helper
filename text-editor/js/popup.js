3d867d9a0ed321944eaab02e0f143cec8c45df2c
mat3049 - Aeolus <mat3049@gmx.com>
2014-12-01 12:33:10 -0500

    Added File API verification
    Added check for browser support for HTML5 File API.
    Added early version of local file input/loading--not yet functional:
    file is selected, but webpage does not respond to the input change.

--- text-editor/js/popup.js
+++ text-editor/js/popup.js
@@ -7,6 +7,11 @@ $.getScript("checkCS502x.js", function () {
 
 $(document).ready(function () {
     "use strict";
+
+    //checkFileAPI();
+
+    $('#loadedcontent').load("test.html");
+
     $('#summernote').summernote({
         height: 300,
         width: 500,
@@ -18,4 +23,35 @@ $(document).ready(function () {
     var button = Dropbox.createSaveButton('summernote', document.getElementById('summernote'));
     document.getElementById("container").appendChild(button);
 
+    document.getElementById('file').addEventListener('change', loadFile, false);
+
 });
+
+// Check for the various File API support.
+function checkFileAPI() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        alert('We good!');
+    } else {
+        alert('The File APIs are not fully supported by your browser.');
+    }
+}
+
+function loadFile(evt) {
+    var f = evt.target.files[0];
+
+    if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+            var contents = e.target.result;
+            alert("Got the file.n"
+                + "name: " + f.name + "n"
+                + "type: " + f.type + "n"
+                + "size: " + f.size + " bytesn"
+                + "starts with: " + contents.substr(1, contents.indexOf("n")));
+        };
+
+        r.readAsText(f);
+    } else {
+        alert("Failed to load file");
+    }
+}
