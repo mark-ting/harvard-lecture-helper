3f95865b356a30be5a230f22010b6c4d54713352
goldenratio1618 <aaatanas@yahoo.com>
2014-12-04 04:56:45 -0500

    Buttons work
    Buttons work to cause CS50 2X or pause-video/get-timestamp to execute.
    Remains to do: CS50 2x resets time with respect to original video
    (probably unavoidable since we don't have API access to original video),
    and timestamps are currently locally stored within  the inner js, not
    html

--- get-flash/js/load.js
+++ get-flash/js/load.js
@@ -1,12 +1,8 @@
-// listen for click
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Code for loading CS50 2X, a prerequisite for pausing videos as FlowPlayer is needed
-    // get current tab
-    // http://stackoverflow.com/a/18436323
+document.getElementById('cs50').addEventListener("click", function () {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
 
-        // only inject into all frames for Canvas (which has iframes)
-        var allFrames = /:\/\/canvas\.harvard\.edu|cm\.dce\.harvard\.edu\//.test(tabs[0].url);
+    // only inject into all frames for Canvas (which has iframes)
+    var allFrames = /:\/\/canvas\.harvard\.edu|cm\.dce\.harvard\.edu\//.test(tabs[0].url);
 
         // inject extension's JavaScript
         chrome.tabs.executeScript(undefined, {allFrames: true, file: 'lib/jquery-1.11.1.min.js'}, function(results) {
@@ -18,14 +14,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
             });
         });
     });
-    
-    // Code specific to pausing video
-    // Only works the second time the button is clicked, so the user can use just CS50 2x if they want to, second click pauses video. Either way, timestamp is generated.
+});
+
+document.getElementById('gettime').addEventListener("click", function () {
     console.log("Injecting video-pausing code...");
     chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/pausevideo.js'});
     console.log("Injected code!");
 });
-
-// add badge to icon
-chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
-chrome.browserAction.setBadgeText({text: '2x'});
