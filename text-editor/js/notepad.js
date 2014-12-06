093326be5012ca60c78aece0ff618dff2ab691a9
mat3049 - SIGMA128 <mat3049@gmx.com>
2014-12-06 15:21:57 -0500

    Notepad now opens in new window
    - Notepad option now creates a new window rather than tab
    - Notepad window now resizes nicely on open
    - Regression: no longer able to load new_user.html on new detected
    localStorage

--- text-editor/js/notepad.js
+++ text-editor/js/notepad.js
@@ -1,67 +1,93 @@
 /*global $, jQuery, alert*/
 
-$(document).ready(function () {
+var extID = chrome.runtime.id;
+
+$(document).ready(function (e) {
     "use strict";
+
+    resizeWindow();
 
     open('#summernote');
     $('#save-note').toggle();
-    $('#load-file').toggle();
-    //toggleOptionVisibility();
 
     // listener for editing Summernote canvas
-    $('#edit-note').click(function () {
+    $('#edit-note').click(function (e) {
         open('#summernote');
     });
 
     // listener for saving Summernote canvas
-    $('#save-note').click(function () {
+    $('#save-note').click(function (e) {
         close('#summernote');
     });
 
+    $('#load-note').click(function (e) {
+        file_import('#summernote');
+    });
+
     // listener for downloading saved HTML canvas
-    $('#download-note').click(function () {
-        var fileName =  document.getElementById('note-title').value; // You can use the .txt extension if you want
+    $('#download-note').click(function (e) {
+        var fileName =  $('#note-title').val(); // You can use the .txt extension if you want
 
         if (fileName !== null) {
-            file_export(fileName, 'summernote', 'text/plain');
+            file_export(fileName, '#summernote', 'text/plain');
         } else {
             alert('Please provide a title for your note!');
         }
     });
-
-    $('#load-note').click(function () {
-        file_import('#summernote');
-    });
-
-    $('#summernote').focusout(function () {
-        localStorage[key] = editableContent.innerHTML;
-    });
 });
 
+// toggles visibility of UI elements
+function toggleOptionVisibility() {
+    $("#edit-note").toggle();
+    $("#save-note").toggle();
+    $("#save-file").toggle();
+    $("#load-file").toggle();
+}
 
 // create new instance of Summernote element within specified element
 function open(element) {
     $(element).summernote({
-        height: 600,
-        width: 1024,
+        height: 480,
+        width: 800,
         minHeight: 400,
-        maxHeight: 768,
+        maxHeight: 640,
         focus: true,
         onkeyup: function(e) {
-            save_local('#summernote', 'notepad');
+            save_local('#summernote', extID);
         },
+        toolbar: [
+            ['style', ['style']], // no style button
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['insert', ['picture', 'link']], // no insert buttons
+            ['table', ['table']], // no table button
+            ['help', ['help']] //no help button
+        ]
     });
-
-    load_local('#summernote', 'notepad');
+    load_local('#summernote', extID);
     toggleOptionVisibility();
 }
 
 // close opened instance Summernote element within specified element
 function close(element) {
-    save_local(element, 'notepad');
+    save_local(element, extID);
     $(element).html = $(element).code();
     $(element).destroy();
     toggleOptionVisibility();
+}
+
+// download dynamic HTML element
+function file_export(fileName, elementID, mimeType) {
+    var elHtml = $(elementID).html(),
+        link = document.createElement('a');
+    mimeType = mimeType || 'text/plain';
+
+    link.setAttribute('download', fileName);
+    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+    link.click();
 }
 
 // saves innerHTML within specified element to localStorage using specified sessionKey
@@ -72,38 +98,20 @@ function save_local(element, sessionKey) {
 
 // loads innerHTML within specified element to localStorage using specified sessionKey
 function load_local(element, sessionKey) {
-    if (localStorage[sessionKey]) {
-        var content = localStorage[sessionKey];
+    var storage = localStorage[sessionKey];
+    if (storage != 'undefined' || storage != 'null') {
+        var content = storage;
     } else {
         var content = $(element).load('new_user.html');
     }
-
     $(element).code(content);
-}
-
-// toggles visibility of UI elements
-function toggleOptionVisibility() {
-    $("#edit-note").toggle();
-    $("#save-note").toggle();
-    $("#save-file").toggle();
-    $("#load-file").toggle();
-}
-
-// download dynamic HTML element
-function file_export(fileName, elementID, mimeType) {
-    var elHtml = document.getElementById(elementID).innerHTML,
-        link = document.createElement('a');
-    mimeType = mimeType || 'text/plain';
-
-    link.setAttribute('download', fileName);
-    link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
-    link.click();
 }
 
 // loads user-provided file (text document) into specified element
 function file_import(element) {
     var file = $('#select-file')[0].files[0];
     var reader = new FileReader();
+
     reader.onload = function () {
         var content = reader.result;
         var target = $(element);
@@ -113,10 +121,21 @@ function file_import(element) {
 
         // load into element
         $(target).html(content);
-    }
+    };
+
     if (file) {
         reader.readAsText(file);
     } else {
         alert('Please choose a file to upload!');
     }
+}
+
+function resizeWindow (e) {
+    chrome.windows.getCurrent(function(window) {
+        var windowSize = {
+            width: 820,
+            height: 670
+        };
+        chrome.windows.update(window.id, windowSize);
+    });
 }
