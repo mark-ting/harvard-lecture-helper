a16371bd5e3cababf0597111ccae44b73f0904fd
goldenratio1618 <aaatanas@yahoo.com>
2014-12-06 16:12:29 -0500

    Misc Bug Fixes, Code Cleanup
    Fixed bugs with getting timestamps multiple times in a row, cleaned up
    all extraneous console.log and alert statements in the code, added new
    alert statements if something went wrong

--- get-flash/js/pausevideo.js
+++ get-flash/js/pausevideo.js
@@ -1,161 +1,93 @@
-// From https://stackoverflow.com/questions/8409577/get-all-the-objects-dom-or-otherwise-using-javascript
-
-// console.log("Entering scripts.js");
-/*var objs = []; // we'll store the object references in this array
-
-function walkTheObject(obj) {
-    var keys = Object.keys(obj); // get all own property names of the object
-
-    keys.forEach(function (key) {
-        var value = obj[key]; // get property value
-
-        // if the property value is an object...
-        if (value && typeof value === 'object') {
-
-            // if we don't have this reference...
-            if (objs.indexOf(value) < 0) {
-                objs.push(value); // store the reference
-                console.log("Type of object: " + typeof value);
-                walkTheObject(value); // traverse all its own properties
-            }
-
-        }
-    });
+function copy(str) {
+    window.prompt("URL to resume playback. To copy: Ctrl-C/Cmd-C + Enter.", str);
 }
 
-walkTheObject(this); // start with the global object
-*/
-
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
-//console.log('Printing elements of ' + tab.url);
-    // document.body.style.backgroundColor="red"
-    // No tabs or host permissions needed!
-        // From https://stackoverflow.com/questions/8409577/get-all-the-objects-dom-or-otherwise-using-javascript
-
-var objs = []; // we'll store the object references in this array
-
+// we're on youtube
 if (document.URL.indexOf("youtube.com") > -1) {
-    //window.location = document.URL + "&enablejsapi=1";
+    // get all video elements
     var videoElements = document.getElementsByTagName('video');
-    /* var id = document.URL.substring(document.URL.indexOf("v=") + 2);
-    id = id.substring(0, id.indexOf("&"));
-    console.log(id);
-    var player = document.getElementById(id); */
     try {
+        // couldn't find video
         if (videoElements === null || videoElements.length === 0) {
             alert("Error: Could not locate video");
         } else {
             for (var i = 0; i < videoElements.length; i++) {
-                console.log(videoElements[i]);
                 // pause video
                 videoElements[i].pause();
                 // time of current video
                 var time = Math.floor(Number(videoElements[i].currentTime));
-                var index = document.URL.indexOf("t=");
+                
+                var index = document.URL.indexOf("&t=");
                 // url to resume playing at current time
+                var url = document.URL;
                 if (index === -1)
-                    var url = document.URL + "&t=" + time;
+                    var url = url + "&t=" + time;
                 else {
-                    var url = document.URL.substring(0, index);
-                    url = url + time + document.URL.substring(url.indexOf("&"));
+                    var after_url = url.substring(index + 4);
+                    var next_var = after_url.indexOf("&");
+                    var url = url.substring(0, index + 3);
+                    if (next_var === -1) {
+                        url = url + time;
+                    } else {
+                        url = url + time + after_url.substring(next_var);
+                    }
                 }
-                console.log(url);
+                copy(url);
             }
         }
-    } catch (err) {console.log("ERROR: " + err); }
+    } catch (err) {console.log("Error: " + err); }
 } else {
-    /* var embedded = document.getElementsByTagName('embed');
-    if (embedded !== null) {
-        try {
-            for (var i = 0; i < embedded.length; i++) {
-                alert("Testing something...");
-                if (embedded[i].type === "application/x-shockwave-flash") {
-                    alert("Testing something else...");
-                    var src = embedded[i].getPlaylist()[0].file;
-                    alert(src);
-                    alert(embedded[i]);
-                    embedded[i].remove();
-                    document.write('<video class="video-stream html5-main-video" x-webkit-airplay="allow" src="' + src + '" style="width: 479px; height: 360px; left: 80.75px; top: 0px; transform: none;"></video>');
-                }
-            }
-        } catch (err) {console.log("ERROR: " + err)}
-    } */
     // Assume CS50 2x has already run, so the player is Flowplayer.
-    //window.location = document.URL + "&enablejsapi=1";
+    // Then we can just get all video elements again
     var videoElements = document.getElementsByTagName('video');
-    /* var id = document.URL.substring(document.URL.indexOf("v=") + 2);
-    id = id.substring(0, id.indexOf("&"));
-    console.log(id);
-    var player = document.getElementById(id); */
     try {
+        // couldn't find video, probably because there isn't one or the user forgot to press CS50 2X
         if (videoElements === null || videoElements.length === 0) {
             alert("Error: Could not locate video to get timestamp. Perhaps you forgot to run CS50 2X?");
         } else {
+            var found = false;
             for (var i = 0; i < videoElements.length; i++) {
+                // some webpages might have multiple videos, so only look at the actively playing one
                 if (!videoElements[i].paused) {
-                    console.log(videoElements[i]);
+                    found = true;
                     // pause video
                     videoElements[i].pause();
                     // time of current video
                     var time = Math.floor(Number(videoElements[i].currentTime));
+                    var index1 = document.URL.indexOf("playbackTime=");
                     // url to resume playing at current time
-                    var url = document.URL + "&playbackTime=" + time + "&videoSource=" + videoElements[i].src;
-                    console.log(url);
+                    var url = document.URL;
+                    if (index1 === -1)
+                        var url = url + "&playbackTime=" + time;
+                    else {
+                        var after_url = url.substring(index1 + 14);
+                        var next_var = after_url.indexOf("&");
+                        var url = url.substring(0, index1 + 13);
+                        if (next_var === -1) {
+                            url = url + time;
+                        } else {
+                            url = url + time + after_url.substring(next_var);
+                        }
+                    }
+                    var index2 = url.indexOf("videoSource=");
+                    if (index2 === -1)
+                        var url = url + "&videoSource=" + videoElements[i].src;
+                    else {
+                        var after_url = url.substring(index2 + 13);
+                        var next_var = after_url.indexOf("&");
+                        var url = url.substring(0, index2 + 12);
+                        if (next_var === -1) {
+                            url = url + videoElements[i].src;
+                        } else {
+                            url = url + videoElements[i].src + after_url.substring(next_var);
+                        }
+                    }
+                    copy(url);
                 }
+            }
+            if (!found) {
+                alert("All videos on this page are paused. Please play the one you wish to take the timestamp of.");
             }
         }
-    } catch (err) {console.log("ERROR: " + err);}
+    } catch (err) {console.log("Error: " + err);}
 }
-    /* function walkTheObject(obj) {
-        try {
-            //console.log("Recursing...");
-            var keys = Object.keys(obj); // get all own property names of the object
-
-            keys.forEach(function (key) {
-                var value = obj[key]; // get property value
-
-                // if the property value is an object...
-                if (value && typeof value === 'object') {
-
-                    // if we don't have this reference...
-                    if (objs.indexOf(value) < 0) {
-                        objs.push(value); // store the reference
-
-                        walkTheObject(value); // traverse all its own properties
-                    }
-
-                }
-            });
-        } catch (err) {console.log("ERROR: " + err); }
-    }
-
-    walkTheObject(document); // start with the global object
-
-    console.log("Trying to find flash files...");
-
-    var flash = null;
-    objs.forEach(function (obj) {
-        try {
-            if (obj.type === "application/x-shockwave-flash") {
-                console.log("Found flash file: " + obj);
-                console.log("Id = " + obj.id);
-                flash = obj;
-            }
-        } catch (err) {console.log("ERROR: " + err); }
-    });
-    console.log(flash);
-    flash.remove();*/
-    /*,                    function(results) {
-             chrome.tabs.executeScript(undefined, {allFrames: true, file:'js/findurl.js'});
-        } );
-    });
-});
-
-
-
-
-    chrome.tabs.executeScript(undefined, {allFrames: true, file: 'js/jquery-1.11.1.min.js'}, function (results) {*/
--- get-flash/js/resumeplayback.js
+++ get-flash/js/resumeplayback.js
@@ -1,31 +1,26 @@
 // don't do this if we're on youtube, which handles its own time separately
 if (document.URL.indexOf("youtube.com") === -1) {
+    // get playback time
     var time = document.URL.substring(document.URL.indexOf("playbackTime=") + 13);
     var nextVar = time.indexOf("&");
     if (nextVar !== -1)
         time = time.substring(0, nextVar);
     // videoSource needs to be last input in URL
     var src = document.URL.substring(document.URL.indexOf("videoSource=") + 12);
-    
+    // get all videos on webpage
     var videoElements = document.getElementsByTagName('video');
-    console.log(time);
-    console.log(src);
-    console.log(videoElements);
-    /* var id = document.URL.substring(document.URL.indexOf("v=") + 2);
-    id = id.substring(0, id.indexOf("&"));
-    console.log(id);
-    var player = document.getElementById(id); */
     try {
+        // no video found
         if (videoElements === null || videoElements.length === 0) {
             alert("No videos are available for resuming on this page. Perhaps you forgot to run CS50 2X?");
         } else {
             for (var i = 0; i < videoElements.length; i++) {
+                // make sure video is the right one, the one specified in src
                 if (videoElements[i].src == src) {
-                    console.log(videoElements[i]);
                     // play video
                     videoElements[i].play();
+                    // set video's time to time stored in URL
                     videoElements[i].currentTime = time;
-                    console.log(time);
                 }
             }
         }
