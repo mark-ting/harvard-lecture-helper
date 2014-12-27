b196633b9853d8b9598004473b894716685f34ec
mat3049 - Aeolus <mat3049@gmx.com>
2014-12-27 15:44:03 -0700

    Working event delegation for main menu
    Working event bubbling assignment for main menu popup.
    Modifications are stored in the popup_fast.js and popup_fast.html pair.
    Also modified manifest.json to utilize faster menu.

--- harvard-lecture-helper/js/popup_fast.js
+++ harvard-lecture-helper/js/popup_fast.js
@@ -3,54 +3,42 @@
 $(document).ready(function () {
     "use strict";
 
-    // listener for notepad window
-    $('#notepad').click(function () {
-         chrome.windows.create({url: "notepad.html"});
-    });
+    $('#main-menu').on('click', 'button, input', function (e) {
+        e.preventDefault();
+        var target = e.target;
 
+        switch (target.id) {
+        case 'notepad':
+            chrome.windows.create({url: "notepad.html"});
+            break;
+        case 'run-cs50-2x':
+            chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+                // only inject into all frames for Canvas (which has iframes)
+                var allFrames = /:\/\/canvas\.harvard\.edu|cm\.dce\.harvard\.edu\//.test(tabs[0].url);
 
-    // NOTE Perhaps the injection is causing the .3 sec slowdown?
-    // TODO Look into simplifying the injection script, or maybe moving it to a separate, call-able JS file?
-    // TODO Ask Adam if the run-time needs to be executed live and per-click on the menu, or just needs to be alive in the background.
-
-    // listener for CS50 2x extension code (Thanks David!)
-    $('#runcs502x').click(function () {
-        chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-
-        // only inject into all frames for Canvas (which has iframes)
-        var allFrames = /:\/\/canvas\.harvard\.edu|cm\.dce\.harvard\.edu\//.test(tabs[0].url);
-
-            // inject extension's JavaScript
-            chrome.tabs.executeScript(undefined, {allFrames: true, file: 'lib/jquery-1.11.1.min.js'}, function(results) {
-                chrome.tabs.executeScript(undefined, {allFrames: true, file: 'lib/flowplayer.commercial-5.5.0/flowplayer.min.js'}, function(results) {
-                    chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/script.js'}, function(results) {
-                        chrome.tabs.executeScript(undefined, {allFrames: true, file: 'js/findurl.js'});
+                // inject extension's JavaScript
+                chrome.tabs.executeScript(undefined, {allFrames: true, file: 'lib/jquery-1.11.1.min.js'}, function (results) {
+                    chrome.tabs.executeScript(undefined, {allFrames: true, file: 'lib/flowplayer.commercial-5.5.0/flowplayer.min.js'}, function (results) {
+                        chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/script.js'}, function (results) {
+                            chrome.tabs.executeScript(undefined, {allFrames: true, file: 'js/findurl.js'});
+                        });
                     });
-                });
+                }); // all credit for CS50 2x goes to its authors
             });
-        });
-    });
-    // all credit for CS50 2x goes to its actual authors
-
-    // listener for video time-grab (and pause!) function
-    $('#gettime').click(function () {
-        console.log('pause video good yo');
-        chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/pausevideo.js'})
-    });
-
-    // listener for function to resume video playback
-    $('#resume').click(function () {
-        console.log('resume video good yo');
-        chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/resumeplayback.js'})
-    });
-
-    // listener for options
-    $('#option').click(function () {
-        chrome.tabs.create({url: "options.html"});
-    });
-
-    // listener for help page
-    $('#help').click(function () {
-        chrome.tabs.create({url: "help.html"});
+            break;
+        case 'get-time':
+            chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/pausevideo.js'});
+            break;
+        case 'resume':
+            chrome.tabs.executeScript(undefined, {allFrames: false, file: 'js/resumeplayback.js'});
+            break;
+        case 'options':
+            chrome.tabs.create({url: "options.html"});
+            break;
+        case 'help':
+            chrome.tabs.create({url: "options.html"});
+            break;
+        default:
+        }
     });
 });
